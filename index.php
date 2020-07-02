73f1a2e8ebcdcac3865fa6342341965b0b5fa235
GAYU997 <67303008+GAYU997@users.noreply.github.com>
2020-07-02 21:45:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+
+<meta charset="utf-g">
+<meta http-equiv="X-UX-Compatible" content="IE-edge">
+<meta name="viewport" content="width-device-width,initial-scale=1">
+<title>WEBSITE</title>
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script>
+<script src="app/route.js"></script>
+<script src="app/controllers/postController.js"></script>
+
+<body ng-app="sample">
+ <center></br>
+ <h1><b>WELCOME GUEST..!!</b></h1>
+ <a href="/AngularForm">HOME</a>
+ <a href="#!menu1">MENU1</a>
+ <div class="ng-view"></div></br>
+ <center>
+ </body>
+</html>
---
+++ postcontroller.js
@@ -0,0 +1,9 @@
+app.controller('myController',['$scope',function($scope)
+{
+    //alert("hai");
+    $scope.click = function(){
+        alert('name=' + $scope.name + ',' + 'email=' + $scope.email);
+    };
+    
+
+}]);
